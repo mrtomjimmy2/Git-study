155f3b7f70aa3f3cd0887284156499e8c8d8cbcb
Tom Jimmy <mrtomjimmy2@gmail.com>
2026-05-19 18:48:22 +0700

    update chuvi and dientich

--- tinh_cv_dt_hcn.py
+++ tinh_cv_dt_hcn.py
@@ -2,10 +2,10 @@
 dai = int(input("Nhap chieu dai: "))
 rong = int(input("Nhap chieu rong: "))
 
-# tinh cv va dt
-cv = (dai + rong) * 2
-dt = dai * rong
+# tinh chuvi va dientich
+chuvi = (dai + rong) * 2
+dientich = dai * rong
 
 # In ket qua ra man hinh
-print("chu vi la: ", cv)
-print("dien tich la: ", dt)
+print("chu vi la: ", chuvi)
+print("dien tich la: ", dientich)
